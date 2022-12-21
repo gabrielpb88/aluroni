98d6440192b23a015cf41c09c1c796271e3279b5
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2022-12-21 18:07:04 -0300

    add Cardapio Component

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,1 +1,11 @@
-
+import s from './Cardapio.module.scss';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className={s.menu}>
+        <Logo />
+      </nav>
+    </main>
+  );
+}
